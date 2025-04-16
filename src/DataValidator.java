0723946c51c11e676787aa8c381b2b4806a6616f
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2025-04-16 15:25:10 +0300

    Added validateAge method

--- src/DataValidator.java
+++ src/DataValidator.java
@@ -9,4 +9,16 @@ public class DataValidator {
         return name;
     }
 
+    private static String validateAge(String age) {
+        // Удаляем все нецифровые символы
+        age = age.replaceAll("[^0-9]", "");
+        try {
+            int ageNum = Integer.parseInt(age);
+            if (ageNum < 0 || ageNum > 120) return "";
+        } catch (NumberFormatException e) {
+            return "";
+        }
+        return age;
+    }
+
 }
